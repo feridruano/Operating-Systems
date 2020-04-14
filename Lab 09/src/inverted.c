8c2d1fd9c0ecf4c4e37db4368ee32f6d45e25bf6
Ferid Ruano <feridruano@gmail.com>
2020-04-13 19:19:15 -0700

    Completed Non-static/dynamic Lab

--- Lab 09/src/inverted.c
+++ Lab 09/src/inverted.c
@@ -5,56 +5,79 @@
 int tableSize;
 int frameSize;
 
-int lookUp(PROC *, int, int);
-int findOldest(PROC *table);
+int lookUp(PROC*, int, int);
+int findOldest(PROC* table);
 
 /*
  * initializes the table for the given sizes of the memory and frame
  * all entries should be zeroed (e.g., calloc())
  */
-void initInverted(PROC **table, int msize, int fsize)
+void initInverted(PROC** table, int msize, int fsize)
 {
-    // TODO: implement
+	// TODO: Done
+	frameSize = fsize;
+	tableSize = msize / fsize;
+	*table = calloc(tableSize, sizeof(PROC));
 }
-
 
 /*
  * translate a logical address <pid, page, offset> into a physical address
  * if there is no entry for the pid and the page number in the table, it has to be added
  * if there is no room in the table for the new entry, then the oldest entry has to be removed
  */
-long translate(PROC *table, int pid, int page, int offset)
+long translate(PROC* table, int pid, int page, int offset)
 {
-    // TODO: implement
+	// TODO: Done
+	int index = lookUp(table, pid, page);
+	if (0 > index)
+	{
+		struct timespec time;
+		clock_gettime(CLOCK_REALTIME, &time);
+		index = findOldest(table);
+		table[index].pid = pid;
+		table[index].page = page;
+		table[index].timeStamp = time.tv_sec * 1000000000 + time.tv_nsec;
+	}
+	return index * frameSize * offset;
 }
 
 //
 // find the entry for a given pid and page
 //
-int lookUp(PROC *table, int pid, int page)
+int lookUp(PROC* table, int pid, int page)
 {
-    // TODO: implement
+	// TODO: Done
+	for (int i = 0; i < tableSize; ++i)
+		if (pid == table[i].pid && page == table[i].page)
+			return i;
+	return -1;
 }
 
 //
 // find the oldest entry (or not used that has the timestamp == 0)
 //
-int findOldest(PROC *table)
+int findOldest(PROC* table)
 {
-    // TODO: implement
+	// TODO: Done
+	time_t oldestTimeStamp = table->timeStamp;
+	for (int pid = 0; pid < tableSize; ++pid)
+		if (0 == oldestTimeStamp)
+			return pid;
+		else if (oldestTimeStamp > table[pid].timeStamp)
+			oldestTimeStamp = table[pid].timeStamp;
+	return oldestTimeStamp;
 }
-
 
 /***
  * prints the inverted table
 */
-void printInverted(PROC *table)
+void printInverted(PROC* table)
 {
-    printf("-------------------------------------\n");
-    printf("        INVERTED TABLE CONTENT\n");
-    printf("-------------------------------------\n");
-    printf("| PID | PAGE |       TIMESTAMP\n");
-    printf("-------------------------------------\n");
-    for (int i = 0; i < tableSize; i++)
-        printf("| %3d | %4d | %20ld |\n", table[i].pid, table[i].page, table[i].timeStamp);
+	printf("-------------------------------------\n");
+	printf("        INVERTED TABLE CONTENT\n");
+	printf("-------------------------------------\n");
+	printf("| PID | PAGE |       TIMESTAMP\n");
+	printf("-------------------------------------\n");
+	for (int i = 0; i < tableSize; i++)
+		printf("| %3d | %4d | %20ld |\n", table[i].pid, table[i].page, table[i].timeStamp);
 }
