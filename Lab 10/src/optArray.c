51b0fb45dc38218e10f85a9dab32ee6ab32e35b0
Ferid Ruano <feridruano@gmail.com>
2020-04-21 17:41:43 -0700

    Lab 10

--- Lab 10/src/optArray.c
+++ Lab 10/src/optArray.c
@@ -10,11 +10,11 @@
 
 #include "../inc/optArray.h"
 
-int *referenceString;
+int* referenceString;
 int refStringLength;
 
 // the page frame list
-int *pageTable;
+int* pageTable;
 int numOfFramesPerProcess;
 
 int victimIndex;
@@ -26,11 +26,33 @@ int numOfFaults;
 // this index is used to go through the sequence of pages references
 int currentPgeReferenceIndex;
 
-int testOPT(int numOfFrames, int *refString, int refStrLen)
+int testOPT(int numOfFrames, int* refString, int refStrLen)
 {
-    // TODO: implement
+	// TODO: Done
+	// Initialize Variables
+	referenceString = refString;
+	refStringLength = refStrLen;
+	numOfFramesPerProcess = numOfFrames;
+	victimIndex = FREE_SLOT;
+	hitPageNumber = FREE_SLOT;
+	pageTable = calloc(numOfFrames, sizeof(int));
 
-    return numOfFaults;
+	// Set Free Slots in pageTable Array
+	for (int frame = 0; frame < numOfFrames; ++frame)
+		pageTable[frame] = FREE_SLOT;
+
+	// Iterate Through Reference String
+	currentPgeReferenceIndex = 0;
+	while (currentPgeReferenceIndex < refStringLength)
+	{
+		insertOPT(referenceString[currentPgeReferenceIndex]);
+		displayOPT();
+		currentPgeReferenceIndex++;
+	}
+
+	// Clean Up
+	freePageTableOPT();
+	return numOfFaults;
 }
 
 /*
@@ -38,9 +60,9 @@ int testOPT(int numOfFrames, int *refString, int refStrLen)
  */
 void insertOPT(int pageNumber)
 {
-    int searchVal = searchOPT(pageNumber);
+	int searchVal = searchOPT(pageNumber);
 
-    pageTable[searchVal] = pageNumber;
+	pageTable[searchVal] = pageNumber;
 }
 
 /*
@@ -48,23 +70,77 @@ void insertOPT(int pageNumber)
  */
 int searchOPT(int pageNumber)
 {
-    // TODO: implement
+	// TODO: Done
+	// Iterate through pageTable Array for pageNumber
+	for (int frame = 0; frame < numOfFramesPerProcess; ++frame)
+	{
+		// Empty Frame with pageNumber Found
+		if (FREE_SLOT == pageTable[frame])
+		{
+			numOfFaults++;
+			victimIndex = frame;
+			return victimIndex;
+		}
 
-    return findVictimPageOPT();
+		// Frame with pageNumber Found
+		if (pageNumber == pageTable[frame])
+		{
+			hitPageNumber = frame;
+			return hitPageNumber;
+		}
+	}
+	numOfFaults++;
+	return findVictimPageOPT();
 }
 
 int findVictimPageOPT()
 {
-    // TODO: implement
-    return 0;
+	// TODO: Test
+	// Task: Replace the page that will not be used for the longest period of time.
+	int found;
+	int longestUnusedTime = 0;
+	for (int frame = 0; frame < numOfFramesPerProcess; ++frame)
+	{
+		found = -1;
+		// Iterate Through Reference String for Longest Unused Period of Time of a Page.
+		for (int refStrIndex = currentPgeReferenceIndex; refStrIndex < refStringLength; ++refStrIndex)
+		{
+			// Found Frame pageNumber in Reference String Again
+			if (pageTable[frame] == referenceString[refStrIndex])
+			{
+				found = 1;
+				// Check for Unused victimIndex or Longer Unused Period of Time
+				if (FREE_SLOT == victimIndex || longestUnusedTime < refStrIndex - currentPgeReferenceIndex)
+				{
+					victimIndex = frame;
+					longestUnusedTime = refStrIndex - currentPgeReferenceIndex;
+				}
+				break;
+			}
+		}
+		if (-1 == found)
+			return frame;
+	}
+	return victimIndex;
 }
 
 void displayOPT()
 {
-    // todo: implement
+	// TODO: Test
+	printf("%d ->", referenceString[currentPgeReferenceIndex]);
+	for (int frame = 0; frame < numOfFramesPerProcess; ++frame)
+	{
+		printf("\t%d", pageTable[frame]);
+		if (hitPageNumber == frame)
+			printf("%c", '<');
+//		else if (FREE_SLOT == hitPageNumber) // Fault
+//			printf("%c", '*');
+	}
+	printf("%c", '\n');
 }
 
 void freePageTableOPT()
 {
-    // TODO: implement
+	// TODO: Done
+	free(pageTable);
 }
